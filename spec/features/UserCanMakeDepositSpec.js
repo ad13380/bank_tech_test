60bf80854cb3b12d9456e1ea0da5af1e8fe21425
Anthony <a.a.donovan@outlook.com>
2020-07-14 11:59:46 +0100

    feature tests added

---
+++ spec/features/UserCanMakeDepositSpec.js
@@ -0,0 +1,18 @@
+describe("Deposit", function() {
+  var header
+  var account
+
+  beforeEach(function() {
+    jasmine.clock().mockDate(new Date(1990, 1, 1));
+    console.log = jasmine.createSpy("log");
+    header = 'date || credit || debit || balance\n'; 
+    account = new Account();
+  });
+
+  it("user can make a deposit", function() {
+    account.deposit(123.4)
+    account.statement
+    const expectedResult = header + '01/02/1990 || 123.40 || || 123.40 \n'
+    expect(console.log).toHaveBeenCalledWith(expectedResult);
+  });
+})
